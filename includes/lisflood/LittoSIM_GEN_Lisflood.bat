a2faad3c9c26622266e6b18467e79286b6512251
Laatabix <laatabi44@gmail.com>
2019-07-18 18:02:42 +0200

    updates

--- includes/lisflood/LittoSIM_GEN_Lisflood.bat
+++
@@ -1,3 +0,0 @@
-cd C:/Users/Laatabi/Desktop/workspace/LittoDev/includes/lisflood/
-lisflood.exe -dir ../../includes/caen/floodfiles/results_R2_t1.562768166618E12 ../../includes/caen/floodfiles/inputs/caen_par_R2_t1.562768166618E12.par
-exit
